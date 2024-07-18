aeae216ad6f1402c646ba90ae29fe39f311d0708
Riccardo Genova <r.genova@bitrocket.dev>
2024-07-18 23:58:29 +0200

    feat: implement base socket chat

---
+++ src/server.ts
@@ -0,0 +1,82 @@
+// #region ::: IMPORTS :::
+import { createClient } from "@vercel/postgres";
+import express, { Request, Response } from "express";
+import { config } from "dotenv";
+import { Server } from "socket.io";
+import { createServer } from "http";
+import cors from "cors";
+import path from "path";
+
+config();
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+const client = createClient({
+  connectionString: process.env.DATABASE_URL,
+});
+
+client.connect();
+
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+  express.json()
+);
+
+app.use(express.static(path.join(__dirname, "public")));
+
+io.on("connection", (socket) => {
+  socket.on("message-sent", (message) => {
+    client.query(
+      `INSERT INTO messages (content) VALUES ($1)`,
+      [message],
+      (error) => {
+        if (!error) io.emit("message-received", message);
+      }
+    );
+  });
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
+});
+
+app.get("/", (req: Request, res: Response) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/api/messages", (req: Request, res: Response) => {
+  client.query("SELECT * FROM messages", (error, response) => {
+    if (error) res.status(500).json({ error });
+    else res.status(200).json(response.rows);
+  });
+});
+
+app.post("/api/messages", (req: Request, res: Response) => {
+  const { content } = req.body;
+  client.query(
+    `INSERT INTO messages (content) VALUES ($1)`,
+    [content],
+    (error) => {
+      if (error) res.status(500).json({ error });
+      else res.status(200).json({ message: "Message created successfully" });
+    }
+  );
+});
+
+server.listen(3000, () => {
+  console.log(`Server SOCKET is running http://localhost:${3000}/socket`);
+});
+
+app.listen(8080, () => {
+  console.log(`Server API is running http://localhost:${8080}`);
+});
